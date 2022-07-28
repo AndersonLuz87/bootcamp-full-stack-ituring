88968a2c26e71f05f54e1e3a23c49b19d7142810
Anderson Luz <andersonluz87@gmail.com>
2022-07-28 19:05:07 -0300

    Finalizar a pratica 4

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-4.js
@@ -0,0 +1,17 @@
+// Pratica 4 .. Convertendo valores (Milles --> Km)
+
+// Declarando as variáveis
+let distanciaLuaTerra = 238900
+let distanciaMilhas = 0
+let distanciaKm = 0
+
+// Transferindo o valor da distanciaLuaTerra para distanciaMilhas
+distanciaMilhas = distanciaLuaTerra
+
+// Convertendo o valor de milhas para Km 
+distanciaKm = distanciaMilhas * 1.60934
+
+// Imprimindo o valor convertido
+console.log(distanciaKm)
+
+
