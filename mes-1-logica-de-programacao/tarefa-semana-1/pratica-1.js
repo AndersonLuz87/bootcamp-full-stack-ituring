0cbd1b457d91369b976bb727d68a9a84e0841eb1
Anderson Luz <andersonluz87@gmail.com>
2022-07-28 16:13:12 -0300

    Declarar variáveis na pratica-1.js

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -0,0 +1,8 @@
+//Passo 1 da Prática 1 (declarando variáveis)
+
+let nome = "Anderson Luz"
+let email = "andersonluz87@gmail.com"
+let profissao = "Técnico Manutenção"
+let idade = 35
+let localDoSonho = "Grécia"
+
