c0e70d7964b727b7f096726b6a06907fee33b106
Anderson Luz <andersonluz87@gmail.com>
2022-07-28 18:42:40 -0300

    Finalizar a pratica 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,24 @@
+// Pratica 3 ... Utilizando IF ELSE
+
+// Declarando as variáveis
+let nome = "Anderson Luz"
+let email = "andersonluz87@gmail.com"
+let profissao = "Técnico Manutenção"
+let idade = 35
+let localDoSonho = "Grécia"
+
+// Imprimindo a comparação do nome
+console.log(typeof nome == "string")
+
+// Imprimindo a comparação do email
+console.log(typeof email == "string")
+
+// Imprimindo a comparação da profissao
+console.log(typeof profissao == "string")
+
+// Imprimindo a comparação da idade
+console.log(typeof idade == "string")
+
+// Imprimindo a comparação do localDoSonho
+console.log(typeof localDoSonho == "string")
+
