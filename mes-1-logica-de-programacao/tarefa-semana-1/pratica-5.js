4398c064706a55ce90f14f022234a1800bea7f49
Anderson Luz <andersonluz87@gmail.com>
2022-07-29 12:12:10 -0300

    Realizar a pratica 5

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,12 @@
+// Pratica 5 .. Praticando o uso do Template String
+
+// Declarando as variaveis utilizadas na Pratica 1 e incluindo a variavel "mensagem"
+let nome = "Anderson Luz"
+let email = "andersonluz87@gmail.com"
+let profissao = "Técnico Manutenção"
+let idade = 35
+let localDoSonho = "Grécia"
+let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + localDoSonho + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+// Imprimindo a mensagem 
+console.log(mensagem)
