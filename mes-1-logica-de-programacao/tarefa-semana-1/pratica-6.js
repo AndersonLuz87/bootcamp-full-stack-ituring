b542aab5e86ef142cc88c745bc2df67b854c4088
Anderson Luz <andersonluz87@gmail.com>
2022-07-29 13:13:13 -0300

    Realizando a pratica 6

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -0,0 +1,46 @@
+// Pratica 6 .. Utilizando função matemática
+
+// Declarando variáveis
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+let numero5 = 5
+let numero6 = 6
+let numero7 = 7
+let numero8 = 8
+let numero9 = 9
+let numero10 = 10
+let resultado1 = 0
+let resultado2 = 0
+let resultado3 = 0
+let resultado4 = 0
+let resultado5 = 0
+let resultado6 = 0
+let resultado7 = 0
+let resultado8 = 0
+let resultado9 = 0
+let resultado10 = 0
+let fator = 123
+
+resultado1 = numero1 * fator
+resultado2 = numero2 * fator
+resultado3 = numero3 * fator
+resultado4 = numero4 * fator
+resultado5 = numero5 * fator
+resultado6 = numero6 * fator
+resultado7 = numero7 * fator
+resultado8 = numero8 * fator
+resultado9 = numero9 * fator
+resultado10 = numero10 * fator
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+console.log(resultado5)
+console.log(resultado6)
+console.log(resultado7)
+console.log(resultado8)
+console.log(resultado9)
+console.log(resultado10)
