f0857e605e447dcc7cc60b9beb635fea741986bb
Anderson Luz <andersonluz87@gmail.com>
2022-07-29 12:22:36 -0300

    Criar o Template e finalizar a prática 5

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -1,12 +1,16 @@
 // Pratica 5 .. Praticando o uso do Template String
 
-// Declarando as variaveis utilizadas na Pratica 1 e incluindo a variavel "mensagem"
+// Declarando as variaveis utilizadas na Pratica 1
 let nome = "Anderson Luz"
 let email = "andersonluz87@gmail.com"
 let profissao = "Técnico Manutenção"
 let idade = 35
 let localDoSonho = "Grécia"
-let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + localDoSonho + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+// Declarando a variavel mensagem e construindo o Template
+let mensagem = `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${localDoSonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
 
 // Imprimindo a mensagem 
 console.log(mensagem)
+
+
