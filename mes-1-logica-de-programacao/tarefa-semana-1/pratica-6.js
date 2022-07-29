c08c9be8ee98125becd67bf88ca1c46539b03993
Anderson Luz <andersonluz87@gmail.com>
2022-07-29 13:31:47 -0300

    Reescrevendo a estrutura do código

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -11,29 +11,19 @@ let numero7 = 7
 let numero8 = 8
 let numero9 = 9
 let numero10 = 10
-let resultado1 = 0
-let resultado2 = 0
-let resultado3 = 0
-let resultado4 = 0
-let resultado5 = 0
-let resultado6 = 0
-let resultado7 = 0
-let resultado8 = 0
-let resultado9 = 0
-let resultado10 = 0
 let fator = 123
+let resultado1 = `${numero1} x ${fator} = ${numero1*fator}`
+let resultado2 = `${numero2*fator}`
+let resultado3 = `${numero3*fator}`
+let resultado4 = `${numero4*fator}`
+let resultado5 = `${numero5*fator}`
+let resultado6 = `${numero6*fator}`
+let resultado7 = `${numero7*fator}`
+let resultado8 = `${numero8*fator}`
+let resultado9 = `${numero9*fator}`
+let resultado10 = `${numero10*fator}`
 
-resultado1 = numero1 * fator
-resultado2 = numero2 * fator
-resultado3 = numero3 * fator
-resultado4 = numero4 * fator
-resultado5 = numero5 * fator
-resultado6 = numero6 * fator
-resultado7 = numero7 * fator
-resultado8 = numero8 * fator
-resultado9 = numero9 * fator
-resultado10 = numero10 * fator
-
+//Imprimindo os resultados  
 console.log(resultado1)
 console.log(resultado2)
 console.log(resultado3)
