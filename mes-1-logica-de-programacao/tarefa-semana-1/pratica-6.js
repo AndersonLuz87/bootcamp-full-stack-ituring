35240254218f23c0fd22ad7a058786b1b4ef7040
Anderson Luz <andersonluz87@gmail.com>
2022-07-29 16:44:25 -0300

    Calcular os resultados e finalizar a pratica 6

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -12,16 +12,17 @@ let numero8 = 8
 let numero9 = 9
 let numero10 = 10
 let fator = 123
+// Declarando as variáveis e calculando os resultados
 let resultado1 = `${numero1} x ${fator} = ${numero1*fator}`
-let resultado2 = `${numero2*fator}`
-let resultado3 = `${numero3*fator}`
-let resultado4 = `${numero4*fator}`
-let resultado5 = `${numero5*fator}`
-let resultado6 = `${numero6*fator}`
-let resultado7 = `${numero7*fator}`
-let resultado8 = `${numero8*fator}`
-let resultado9 = `${numero9*fator}`
-let resultado10 = `${numero10*fator}`
+let resultado2 = `${numero2} x ${fator} = ${numero2*fator}`
+let resultado3 = `${numero3} x ${fator} = ${numero3*fator}`
+let resultado4 = `${numero4} x ${fator} = ${numero4*fator}`
+let resultado5 = `${numero5} x ${fator} = ${numero5*fator}`
+let resultado6 = `${numero6} x ${fator} = ${numero6*fator}`
+let resultado7 = `${numero7} x ${fator} = ${numero7*fator}`
+let resultado8 = `${numero8} x ${fator} = ${numero8*fator}`
+let resultado9 = `${numero9} x ${fator} = ${numero9*fator}`
+let resultado10 = `${numero10} x ${fator} = ${numero10*fator}`
 
 //Imprimindo os resultados  
 console.log(resultado1)
