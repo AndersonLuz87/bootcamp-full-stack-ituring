b4e382db55197a0bc528935d7f7ef5388868bfa3
Anderson Luz <andersonluz87@gmail.com>
2022-08-03 17:57:58 -0300

    Finalizando a prática 4

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-4.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-4.js
@@ -3,12 +3,13 @@
 Criando a função calcularValor que calcula o valor total do produto (valor produto - desconto %)*/
 function calcularValor (valorProduto, desconto) {
   
-    valorTotal = valorProduto * ((100 - desconto) / 100)
+    valorTotal = valorProduto * ((100 - desconto) / 100) /*Calculando o valor total do produto*/
+    console.log(`O valor a ser pago pelo cliente é R$ ${valorTotal} reais`) /*Imprimindo o valor total no terminal*/
   return calcularValor 
 }
 
-
-calcularValor(100,10) /*Chamando a função com os valores a serem calculados*/
-console.log(`O valor a ser pago pelo cliente é R$ ${valorTotal} reais`) /*Imprimindo o valor total no terminal*/
-
-
+calcularValor(100,10) // Chamando a função com valorProduto = 100 e desconto = 10
+calcularValor(100,20) // Chamando a função com valorProduto = 100 e desconto = 20
+calcularValor(100,30) // Chamando a função com valorProduto = 100 e desconto = 30
+calcularValor(100,40) // Chamando a função com valorProduto = 100 e desconto = 40
+calcularValor(100,50) // Chamando a função com valorProduto = 100 e desconto = 50
