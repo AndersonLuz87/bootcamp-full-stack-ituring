07c2794ec9a15d5a45d3ff471375f7c81c563a32
Anderson Luz <andersonluz87@gmail.com>
2022-08-03 00:30:31 -0300

    Atualizando versão

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-1.js
@@ -1,7 +1,9 @@
 //Prática 1 .. Criando uma função
 
+//Criando a função helloworld - Ela irá imprimir na tela a expressão Hello World! :D
 function helloWorld () {
     console.log("Hello World! :D")
 }
 
+//Chamando a função
 helloWorld()
