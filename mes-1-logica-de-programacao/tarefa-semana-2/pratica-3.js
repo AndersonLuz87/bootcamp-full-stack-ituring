855c1c97587a5ae6ab02ea418febc772d71ab27e
Anderson Luz <andersonluz87@gmail.com>
2022-08-02 23:57:04 -0300

    Criando lógica para numero aleatorio Pratica 3

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-3.js
@@ -0,0 +1,11 @@
+//Prática 3: Funções sem parâmetros e retornando valores
+
+//Criando uma função sem parametro
+
+function numeroAleatorio (){
+  return Math.random ()*(10000 - 0) + 0; 
+}
+
+console.log(numeroAleatorio ())
+
+
