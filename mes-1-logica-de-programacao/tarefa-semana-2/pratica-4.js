7e771ea2603c011f586a91161c9e97835f763ed8
Anderson Luz <andersonluz87@gmail.com>
2022-08-03 01:35:40 -0300

    Criando função com parametros para calcular desconto e retornando os valores, prática 4 finalizada

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-4.js
@@ -0,0 +1,14 @@
+/*Prática 4: Funções com parâmetros e retornando valores
+
+Criando a função calcularValor que calcula o valor total do produto (valor produto - desconto %)*/
+function calcularValor (valorProduto, desconto) {
+  
+    valorTotal = valorProduto * ((100 - desconto) / 100)
+  return calcularValor 
+}
+
+
+calcularValor(100,10) /*Chamando a função com os valores a serem calculados*/
+console.log(`O valor a ser pago pelo cliente é R$ ${valorTotal} reais`) /*Imprimindo o valor total no terminal*/
+
+
