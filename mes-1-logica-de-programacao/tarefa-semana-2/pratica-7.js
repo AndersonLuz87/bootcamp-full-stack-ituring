f9eeba27aab6261705d0c57c41a2d875751427e2
Anderson Luz <andersonluz87@gmail.com>
2022-08-04 19:18:29 -0300

    Treinando condicionais, finalizando prática 7

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,17 @@
+//Prática 7: Tradutor de "Hello World!"
+
+function queIdiomaEh (idioma) {
+    if (idioma === "en") {
+        console.log ("Hello World!")
+} else if (idioma === "es") {
+        console.log("Hola mundo!")
+} else {
+        console.log("Olá mundo!")
+}
+}
+
+queIdiomaEh ("en")
+queIdiomaEh ("es")
+queIdiomaEh ("pt")
+queIdiomaEh (" ")
+
