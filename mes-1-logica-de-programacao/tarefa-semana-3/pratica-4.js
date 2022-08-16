36bbf75ded11737114e6fc70f6954f009e836e14
Anderson Luz <andersonluz87@gmail.com>
2022-08-16 19:19:18 -0300

    Escrevendo uma lista como parametro de função, pratica 4 finalizada

--- mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
@@ -1,10 +1,10 @@
 //Prática 4: Minha lista de seriados
 
-function listaSeries (){
+function listaSeries (lista, indice){
+    console.log(`Olá, temos as seguintes séries: ${lista}, você quer escolher uma?`)
 
-    let listaSeries = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
-    return listaSeries
+    return lista [indice]
 }
 
-let exibirLista = listaSeries()
-console.log(exibirLista)
+let exibirLista = listaSeries( ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"],0)
+console.log(`Você escolheu a série ${exibirLista} para assistir!`)
