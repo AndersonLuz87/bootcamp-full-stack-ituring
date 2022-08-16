a2c5241d700376551c27d5deafea24515222d43b
Anderson Luz <andersonluz87@gmail.com>
2022-08-16 00:17:13 -0300

    Trabalhando com lista

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
@@ -0,0 +1,10 @@
+//Prática 4: Minha lista de seriados
+
+function listaSeries (){
+
+    let listaSeries = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+    return listaSeries
+}
+
+let exibirLista = listaSeries()
+console.log(exibirLista)
