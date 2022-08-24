912d6c40bdfcd18639c562ff7bbd7fb9ac32da0f
Anderson Luz <andersonluz87@gmail.com>
2022-08-24 19:16:32 -0300

    Utilizando o metodo .foreach numa array com 7 objetos e retornando 3 operações aritméticas

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-11.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-11.js
@@ -1,26 +1,24 @@
-//
+//Prática 11: Calculando um extrato do banco simples
 
 function calcularSaldo (objeto){
-    let listaFinal = [0,0,0,0,0]
+    let listaFinal = [0,0,0]
 
     objeto.forEach(function (objeto, item) {
 
-        if (objeto.valor >= 0){
-            listaFinal[2] = listaFinal[2] + objeto.valor           
-            listaFinal[0] ++
+        if (objeto.movimentacao === "deposito"){
+            listaFinal[0] = listaFinal[0] + objeto.valor           
+            
         } else {
-            listaFinal[3] = listaFinal[3] + objeto.valor       
-            listaFinal[1] ++
+            listaFinal[1] = listaFinal[1] + objeto.valor       
+         
         }       
-          listaFinal[4] = listaFinal[2] + listaFinal[3]
+          listaFinal[2] = listaFinal[0] - listaFinal[1]
     });
 
     return `
-    Total de depósitos: ${listaFinal[0]}
-    Total de retiradas: ${listaFinal[1]}
-    O valor total depositado foi de: R$ ${listaFinal[2]}
-    O valor total retirado foi de: R$ ${listaFinal[3]}
-    O saldo final de sua conta foi positivo no valor de R$ ${listaFinal[4]}`
+    O valor total depositado foi de: R$ ${listaFinal[0]}
+    O valor total retirado foi de: R$ ${listaFinal[1]}
+    O saldo final de sua conta foi positivo no valor de R$ ${listaFinal[2]}`
     
 }
 
@@ -30,12 +28,12 @@ let calculoDoValor = calcularSaldo([{
     dataMovimentacao: "2021-08-02T07:46:36.611Z"
   },
   {
-    valor: -20.00,
+    valor: 20.00,
     movimentacao: "retirada",
     dataMovimentacao: "2021-09-17T09:46:36.611Z"
   },
   {
-    valor: -30.00,
+    valor: 30.00,
     movimentacao: "retirada",
     dataMovimentacao: "2022-02-24T09:46:36.611Z"
   },
@@ -45,17 +43,17 @@ let calculoDoValor = calcularSaldo([{
     dataMovimentacao: "2022-03-02T09:46:36.611Z"
   },
   {
-    valor: -7.00,
+    valor: 7.00,
     movimentacao: "retirada",
     dataMovimentacao: "2022-04-20T09:46:36.611Z"
   },
   {
-    valor: -21.00,
+    valor: 21.00,
     movimentacao: "retirada",
     dataMovimentacao: "2022-03-20T09:46:36.611Z"
   },
   {
-    valor: -5.00,
+    valor: 5.00,
     movimentacao: "retirada",
     dataMovimentacao: "2022-06-08T09:46:36.611Z"
   }])
