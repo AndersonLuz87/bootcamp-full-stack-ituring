060580a0aa20d6aa8ceba7d9d657befcd4405330
Anderson Luz <andersonluz87@gmail.com>
2022-10-10 20:06:51 -0300

    Finalizando a tarefa 1 - Exibindo o tipo da variável

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -0,0 +1,19 @@
+//Prática 1: Exibindo o tipo da variável
+
+const nome = "Anderson"
+const email = "andersonluz87@gmail.com"
+const profissao = "Empresário"
+const idade = 35  
+const viagemDosSonhos = "Caribe"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagemDosSonhos)
+
+console.log(typeof nome)
+console.log(typeof email)
+console.log(typeof profissao)
+console.log(typeof idade)
+console.log(typeof viagemDosSonhos)
