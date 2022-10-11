d59b46029f620456d11a13416088441d0a497349
Anderson Luz <andersonluz87@gmail.com>
2022-10-10 21:07:31 -0300

    Finalizando a tarefa 3, comparando os tipos das variáveis

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,13 @@
+//Prática 3: Verificando se é uma string ou não
+
+const nome = "Anderson"
+const email = "andersonluz87@gmail.com"
+const profissao = "Empresário"
+const idade = 35  
+const viagemDosSonhos = "Caribe"
+
+console.log(typeof nome == "string")
+console.log(typeof email == "string")
+console.log(typeof profissao == "string")
+console.log(typeof idade == "string")
+console.log(typeof viagemDosSonhos == "string")
