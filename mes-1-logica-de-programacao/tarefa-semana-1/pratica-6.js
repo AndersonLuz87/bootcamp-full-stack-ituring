43cd34f5aab4bbb1b7a88f34b02d7a533ffeec96
Anderson Luz <andersonluz87@gmail.com>
2022-10-11 21:50:22 -0300

    Finalizando a prática 6, tabuada do numero 123

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -0,0 +1,25 @@
+//Prática 6: Exibindo a tabuada de 123
+
+let numero1 = 1;
+let numero2 = 2;
+let numero3 = 3;
+let numero4 = 4;
+let numero5 = 5;
+let numero6 = 6;
+let numero7 = 7;
+let numero8 = 8;
+let numero9 = 9;
+let numero10 = 10;
+let tabuada = 123;
+
+console.log(numero1 * tabuada);
+console.log(numero2 * tabuada);
+console.log(numero3 * tabuada);
+console.log(numero4 * tabuada);
+console.log(numero5 * tabuada);
+console.log(numero6 * tabuada);
+console.log(numero7 * tabuada);
+console.log(numero8 * tabuada);
+console.log(numero9 * tabuada);
+console.log(numero10 * tabuada);
+
