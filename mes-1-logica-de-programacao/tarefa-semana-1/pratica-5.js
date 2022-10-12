b156af7e631b9ee94edc871f5516612b884ee133
Anderson Luz <andersonluz87@gmail.com>
2022-10-11 21:44:56 -0300

    Finalizando prática 5, utilizando Template String

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,9 @@
+//Prática 5: Preparando uma mensagem de WhatsApp com JavaScript
+
+const nome = "Anderson"
+const email = "andersonluz87@gmail.com"
+const profissao = "Empresário"
+const idade = 35  
+const viagemDosSonhos = "Caribe"
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagemDosSonhos}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-1.js
+++
@@ -1,9 +0,0 @@
-//Prática 1 .. Criando uma função
-
-//Criando a função helloworld - Ela irá imprimir na tela a expressão Hello World! :D
-function helloWorld () {
-    console.log("Hello World! :D")
-}
-
-//Chamando a função
-helloWorld()
