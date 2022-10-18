780ab796cf348c1377396a9b9cd1219eb284ee62
Anderson Luz <andersonluz87@gmail.com>
2022-10-18 14:12:19 -0300

    Finalizando a prática 13, operador ternário

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-13.js
@@ -0,0 +1,10 @@
+//Prática 13: Aprendendo o operador ternário
+
+function numeroMaior (primeiroNumero, segundoNumero){
+
+   const n = primeiroNumero > segundoNumero ? primeiroNumero : segundoNumero;
+
+    return n;
+}
+
+console.log(numeroMaior (30,20)); 
