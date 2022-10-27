b0c9e79a7e6e18a880ec718f2edf657c50727042
Anderson Luz <andersonluz87@gmail.com>
2022-10-27 16:33:58 -0300

    Buscando séries de uma array com o método .filter()

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-3.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-3.js
@@ -1,28 +1,5 @@
 //Prática 3: Utilizando o método .filter()
 
-/*function buscarSeriados (lista, busca) {
-
-    let i1 = 0;
-    let i2 = 0;
-    let resultadoDaBusca = [];
-    
-    for (item of lista) {
-
-        if (lista[i1].includes(busca)){
-
-            resultadoDaBusca[i2] = lista[i1]
-            i2 ++;
-        };    
-        
-        i1 ++
-
-    };
-
-    return resultadoDaBusca;
-};
-
-console.log(buscarSeriados(["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"], "Th"));*/
-
 function buscarSeriados (lista,busca){
     let resultadoDaBusca = lista.filter(function(item){
         return item.includes(busca);
