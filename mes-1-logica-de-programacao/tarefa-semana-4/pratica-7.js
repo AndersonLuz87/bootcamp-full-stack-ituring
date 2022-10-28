1f25014d6702ddc56e3774c616144e725eff4cf4
Anderson Luz <andersonluz87@gmail.com>
2022-10-28 19:22:56 -0300

    Função para retornar as chaves de um objeto

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-7.js
@@ -0,0 +1,16 @@
+//Prática 7: Descobrindo as chaves do objeto
+
+function exibeChave (objeto){
+   let chaveObjeto = Object.keys(objeto)
+   return chaveObjeto;
+}
+
+let objeto1 = exibeChave({
+    marca: "Tramontina",
+    produto: "Jogo de talheres",
+    preco: 500,
+    quantidade: 132,
+    categoria: "Utensilhos domésticos"
+  });
+
+  console.log(objeto1);
