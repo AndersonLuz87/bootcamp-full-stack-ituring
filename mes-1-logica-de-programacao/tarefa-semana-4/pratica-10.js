e0fa3275b3bfa2a33e14bcc5e62ddcf074699927
Anderson Luz <andersonluz87@gmail.com>
2022-11-01 23:16:04 -0300

    Somando as propriedades dos objetos de uma array e retornando o valor final

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-10.js
@@ -0,0 +1,52 @@
+//Prática 10: Calculando o saldo final
+
+function calcularSaldo (objeto){
+    let saldo = null
+    objeto.forEach(function (item) {
+        if (item.movimentacao === "deposito")  
+            saldo = saldo + item.valor
+         else 
+            saldo = saldo - item.valor
+        
+    });
+
+    return `O saldo final de sua conta foi R$ ${saldo} reais.`
+}
+
+let saldoFinal = calcularSaldo([{
+    valor: 100.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2021-08-02T07:46:36.611Z"
+  },
+  {
+    valor: 20.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2021-09-17T09:46:36.611Z"
+  },
+  {
+    valor: 30.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-02-24T09:46:36.611Z"
+  },
+  {
+    valor: 10.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2022-03-02T09:46:36.611Z"
+  },
+  {
+    valor: 7.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-04-20T09:46:36.611Z"
+  },
+  {
+    valor: 21.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-03-20T09:46:36.611Z"
+  },
+  {
+    valor: 5.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-06-08T09:46:36.611Z"
+  }]);
+
+  console.log(saldoFinal);
