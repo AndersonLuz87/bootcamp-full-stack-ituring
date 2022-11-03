2c72891d8d421f18c8c830174504c7ada733d647
Anderson Luz <andersonluz87@gmail.com>
2022-11-03 19:34:06 -0300

    Convertendo strings de uma propriedade dos objetos de uma array e ordenado os valores em ordem decrescente

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-13.js
@@ -0,0 +1,50 @@
+//Prática 13: Ordenando movimentações bancárias por data
+
+function ordenarExtrato (objeto){
+  
+        objeto.forEach(function (item) {
+            item.dataMovimentacao = new Date (item.dataMovimentacao);
+        });
+
+        let extrato = objeto.sort((a,b) =>  b.dataMovimentacao - a.dataMovimentacao);
+
+        return extrato;
+}
+
+let extratoOrdenado = ordenarExtrato([{
+    valor: 100.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2021-08-02T07:46:36.611Z"
+  },
+  {
+    valor: 20.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2021-09-17T09:46:36.611Z"
+  },
+  {
+    valor: 30.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-02-24T09:46:36.611Z"
+  },
+  {
+    valor: 10.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2022-03-02T09:46:36.611Z"
+  },
+  {
+    valor: 7.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-04-20T09:46:36.611Z"
+  },
+  {
+    valor: 21.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-03-20T09:46:36.611Z"
+  },
+  {
+    valor: 5.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-06-08T09:46:36.611Z"
+  }]);
+
+  console.log(extratoOrdenado);
